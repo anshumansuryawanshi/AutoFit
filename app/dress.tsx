71e6a8339753424f6a35f27d1cb9bd1f48bc5878
raki-krishnan <raki@umich.edu>
2024-05-25 15:54:40 -0400

    dress.tsx proportionalized

--- app/dress.tsx
+++ app/dress.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView, Image, useColorScheme} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { Dimensions } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 type RootStackParamList = {
     index: undefined;
@@ -12,101 +14,102 @@ type RootStackParamList = {
 };
 type NavigationProp = StackNavigationProp<RootStackParamList, 'dress'>;
 
+const { width, height } = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-      backgroundColor: 'white',
-    },
-    headercontainer: {
-      justifyContent: 'space-between', 
-      backgroundColor: 'white',
-      flexDirection: 'row',
-    },
-    weathercontainer: {
-      paddingLeft: 20
-    },
-    weathertext:{
-      textAlign: 'center',
-      fontSize: 20,
-      paddingTop: 5
-    },
-    personalizecontainer:{
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingRight: 20
-  
-    },
-    personalizetext:{
-      fontSize: 15,
-      color: 'white',
-    },
-    personalizebutton:{
-      width: 100,
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 10,
-      backgroundColor: 'black',
-      borderRadius: 10,
-    },
-    middlecontainer: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    middletext:{
-      fontSize: 60,
-      color: 'black',
-    },
-    footercontainer: {
-      justifyContent: 'flex-end', // Align items to the bottom of the container
-      backgroundColor: 'white',
-    },
-    buttonsContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      width: '100%',
-      padding: 18, 
-      backgroundColor: 'black', // Black background for the buttons area
-    },
-    rightbutton: {
-      padding: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flex: 1, // Allow buttons to expand equally
-      paddingLeft: 100,
-    },
-    leftbutton: {
-      padding: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      flex: 1, // Allow buttons to expand equally
-      paddingRight: 100,
-    },
-    rectangleText: {
-      color: 'white',
-      fontSize: 18
-    },
-    circleText: {
-      color: 'white',
-      textAlign: 'center',
-      fontSize: 40,
-      padding: 20
-    },
-    dressButton: {
-      width: 190,
-      height: 190,
-      backgroundColor: '#E25D61',
-      borderRadius: 95,
-      position: 'absolute',
-      bottom: 0, 
-      alignSelf: 'center', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-    },
-  });
+  container:{
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  headercontainer: {
+    justifyContent: 'space-between', 
+    backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  weathercontainer: {
+    paddingLeft: RFValue(20, 812)
+  },
+  weathertext:{
+    textAlign: 'center',
+    fontSize: RFValue(20, 812),
+    paddingTop: RFValue(5, 812)
+  },
+  personalizecontainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingRight: RFValue(20, 812)
+  },
+  personalizetext:{
+    fontSize: RFValue(15, 812),
+    color: 'white',
+  },
+  personalizebutton:{
+    width: RFValue(100, 812),
+    height: RFValue(50, 812),
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: RFValue(10, 812),
+    backgroundColor: 'black',
+    borderRadius: RFValue(10, 812),
+  },
+  middlecontainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: RFValue(10, 812),
+  },
+  middletext:{
+    fontSize: RFValue(60, 812),
+    color: 'black',
+  },
+  footercontainer: {
+    justifyContent: 'flex-end', // Align items to the bottom of the container
+    backgroundColor: 'white',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    padding: RFValue(18, 812), 
+    backgroundColor: 'black', // Black background for the buttons area
+  },
+  rightbutton: {
+    padding: RFValue(10, 812),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1, 
+    paddingLeft: RFValue(100, 812),
+  },
+  leftbutton: {
+    padding: RFValue(10, 812),
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1, 
+    paddingRight: RFValue(100, 812),
+  },
+  rectangleText: {
+    color: 'white',
+    fontSize: RFValue(20, 812)
+  },
+  circleText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: RFValue(50, 812),
+    padding: RFValue(20, 812)
+  },
+  dressButton: {
+    width: width * 0.5,
+    height: width * 0.5,
+    backgroundColor: '#E25D61',
+    borderRadius: width * 0.25,
+    position: 'absolute',
+    bottom: 0, 
+    alignSelf: 'center', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+  },
+});
   
   export default function Index() {
     const navigation = useNavigation<NavigationProp>();
@@ -134,7 +137,7 @@ const styles = StyleSheet.create({
             <Text style={styles.middletext}>Today's Fit:</Text>
             <Image source={require("../assets/images/rain.png")} style={{ width: 150, height: 150 }}/>
             <Image source={require("../assets/images/thunderstorm.png")} style={{ width: 150, height: 150 }}/>
-            <Image source={require("../assets/images/thunderstorm.png")} style={{ width: 150, height: 150 }}/>
+            <Image source={require("../assets/images/snow.png")} style={{ width: 150, height: 150 }}/>
           </View>
           
           <View style={styles.footercontainer}>
